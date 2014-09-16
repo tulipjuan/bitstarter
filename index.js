b35e2245078d39bd97dbab39b0ebb4e73bee62b5
tulipjuan <amy.li2014@hotmail.com>
2014-09-16 04:51:40 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express();
+var fs = require("fs")
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    response.send(fs.readFileSync('index.html','utf8' ))
 })
 
 app.listen(app.get('port'), function() {
